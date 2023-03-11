9785e8e5f02e18f1f42233042ed4af68878055b7
petrosAth <develop@athanasoulis.me>
2023-03-11 21:39:26 +0200

    Remove debugging console log

--- src/modules/toggleBackground.js
+++ src/modules/toggleBackground.js
@@ -1,17 +1,16 @@
 const toggleBackground = (() => {
-  let counter = 1;
+  let counter = 0;
   let color;
 
   const toggle = () => {
     if (counter % 2 == 0) {
-      color = 'rgb(141, 214, 249)';
-    } else {
       color = 'rgb(29, 120, 193)';
+    } else {
+      color = 'rgb(141, 214, 249)';
     }
 
     document.body.style.background = color;
-    counter = counter + 1;
-    console.log(counter);
+    counter++;
   };
 
   return toggle;
